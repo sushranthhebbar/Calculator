b058c47bf42eae6d71918a0280060ba04ee529e0
Sushranth Hebbar <sushranthhebbar@gmail.com>
2021-03-05 20:35:46 +0530

    Changed relative path to absolute in delivery.sh

--- delivery.sh
+++ delivery.sh
@@ -1,4 +1,4 @@
 set -x
 
-java -cp target/Calculator-1.0-SNAPSHOT.jar Calculator
+java -cp home/sushranth/Downloads/Software_Production/Calculator/target/Calculator-1.0-SNAPSHOT.jar Calculator
 set +x
