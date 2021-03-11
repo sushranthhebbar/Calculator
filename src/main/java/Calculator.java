1dc27e00768021aafd8e4a746a0d95c4b8476221
Sushranth Hebbar <sushranthhebbar@gmail.com>
2021-03-11 18:05:31 +0530

    Restarted

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,16 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculator {
+
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
+    public Calculator(){
+
+    }
 
     public static int factorial(int number)
     {
+        logger.info("[MULTIPLICATION] - " + number);
         int result = 1;
         for(int i=1;i<=number;i++)
         {
@@ -11,6 +20,7 @@ public class Calculator {
     }
     public static void main(String[] args)
     {
+        Calculator calc = new Calculator();
         System.out.println(factorial(10));
     }
 }
