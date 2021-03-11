90ada4a1742103034955c71af2e71b4adf318436
Sushranth Hebbar <sushranthhebbar@gmail.com>
2021-03-11 22:46:38 +0530

    Added testcases for pow

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -26,14 +26,45 @@ public class CalculatorTest {
 
     @Test
     public void trueFactorial() {
-        assertEquals("Square root of positive integer ",3628800,calc.factorial(10),DELTA);
-        assertEquals("Square root of negative integer ", -1,calc.factorial(-200),DELTA);
+        assertEquals("Factorial of positive integer ",3628800,calc.factorial(10),DELTA);
+        assertEquals("Factorial of negative integer ", -1,calc.factorial(-200),DELTA);
     }
 
     @Test
     public void falseFactorial() {
-        assertNotEquals("Square root of positive integer ",29,calc.factorial(5),DELTA);
-        assertNotEquals("Square root of negative integer ", 120,calc.factorial(-11),DELTA);
+        assertNotEquals("Factorial of positive integer ",29,calc.factorial(5),DELTA);
+        assertNotEquals("Factorial of negative integer ", 120,calc.factorial(-11),DELTA);
     }
 
+    @Test
+    public void trueLogarithm() {
+        assertEquals("Logarithm of positive integer ",5.06890420222,calc.log(159),DELTA);
+        assertEquals("Logarithm of positive floating point number ", 3.10897166916,calc.log(22.398),DELTA);
+        assertEquals("Logarithm of positive floating point number less than one", -0.25605421477,calc.log(0.7741),DELTA);
+        assertEquals("Logarithm of negative real number ", NaN,calc.log(-702.922),DELTA);
+    }
+
+    @Test
+    public void falseLogarithm() {
+        assertNotEquals("Logarithm of positive integer ",13,calc.log(78),DELTA);
+        assertNotEquals("Logarithm of positive floating point number ", NaN,calc.log(169.961),DELTA);
+        assertNotEquals("Logarithm of positive floating point number less than one ", 3,calc.log(0.451382),DELTA);
+        assertNotEquals("Logarithm of negative real number ", 62.301,calc.log(-0.00001578),DELTA);
+    }
+
+    @Test
+    public void truePower() {
+        assertEquals("Power of positive base and exponent ",1024,calc.pow(2, 10),DELTA);
+        assertEquals("Power of positive base and negative exponent", 0.20804531060022668,calc.pow(3.7, -1.2),DELTA);
+        assertEquals("Power of negative base and positive exponent", 0.0772628412137455,calc.pow(-0.7741, 10),DELTA);
+        assertEquals("Power of negative base and exponent ", 1,calc.pow(-1, -2),DELTA);
+    }
+
+    @Test
+    public void falsePower() {
+        assertNotEquals("Power of positive base and exponent ",NaN,calc.pow(18, 1.2),DELTA);
+        assertNotEquals("Power of positive base and negative exponent ", NaN,calc.pow(19.961, -0.05),DELTA);
+        assertNotEquals("Power of negative base and positive exponent ", 3,calc.pow(-7.8, 2.3),DELTA);
+        assertNotEquals("Power of negative base and exponent ", 62.301,calc.pow(-13, -6),DELTA);
+    }
 }
